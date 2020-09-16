aa9b2d7577589ad5946c2831cd8e53e360cb00de
Bryce Moore <brycemooore@gmail.com>
2020-09-16 20:54:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,8 +36,8 @@ def monopoly_with_third_tier
   
   hash[:railroads][:names] ={
     :reading_railroad => {},
-    :pennsylvania_railroad => {}
-    :b_and_o_railroad => {}
+    :pennsylvania_railroad => {},
+    :b_and_o_railroad => {},
     :shortline_railroad => {}
   }
   hash
